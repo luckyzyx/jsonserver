017e1d537851c316a0d9cfa5bb77ca4f70febd43
luckyzyx <2813467705@qq.com>
2020-07-11 17:18:12 +0800

    push

--- mock.js
+++ mock.js
@@ -1,20 +1,22 @@
-var Mock = require('mockjs')
-var fs = require('fs')
-var Random = Mock.Random
+var Mock = require("mockjs");
+var fs = require("fs");
+var Random = Mock.Random;
 //保存数据
-var arr = []
+var data = [];
 //动态生成4W条数据
 for (let i = 1; i < 100; i++) {
     //生成随机项
     let name = Random.cname();
-    let age = Mock.mock({ "age|1-100": 100 }).age
+    let age = Mock.mock({ "age|1-100": 100 }).age;
     let address = Random.province();
     //1男2女
     let gender = Random.pick(["1", "2"]);
-    let education = Random.pick(["幼儿园","小学","初中", "高中", "专科", "本科"]);
-    arr.push({ "id": i, name, age, address, gender, education })
+    let education = Random.pick(["1", "2", "3", "4", "5", "6"]);
+
+    // ["幼儿园", "小学", "初中", "高中", "专科", "本科"]
+    data.push({ id: i, name, age, address, gender, education });
 }
 //写入文件
-fs.writeFile("db.json", JSON.stringify({ "users": arr }), function () {
-    console.log("done")
-})
+fs.writeFile("db.json", JSON.stringify({ users: data }), function () {
+    console.log("done");
+});
