9974b47f544cc4e7cfa7ad7d846ab91ba4ab5a11
luckyzyx <2813467705@qq.com>
2022-07-26 23:33:20 +0800

    update version

--- mock.js
+++ mock.js
@@ -4,17 +4,19 @@ var Random = Mock.Random;
 //保存数据到data
 var data = [];
 //动态生成N条数据
-for (let i = 1; i < 100; i++) {
-    //生成随机项
-    let name = Random.cname();
-    let age = Mock.mock({ "age|1-100": 100 }).age;
-    let address = Random.province();
-    //1男2女
-    let gender = Random.pick(["1", "2"]);
-    let education = Random.pick(["1", "2", "3", "4", "5", "6"]);
-    // ["幼儿园", "小学", "初中", "高中", "专科", "本科"]
-    data.push({ id: i, name, age, address, gender, education });
+for (let i = 0; i <= 100; i++) {
+  //生成随机项
+
+  let city = Random.province();
+  let student = {
+    id: i,
+    name: Random.cname(),
+    age: Mock.mock({ "age|1-100": 10 }).age,
+    sex: Random.pick(["男", "女"]),
+  };
+
+  data.push({ city, student });
 }
+
 //写入文件
-fs.writeFile("db.json", JSON.stringify({ users: data }), function () {
-});
+fs.writeFile("db.json", JSON.stringify({ data }), function () {});
